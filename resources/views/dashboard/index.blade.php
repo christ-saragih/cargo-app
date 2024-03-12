5b0ecea8985d94589b2f7a45a440e343c8033012
christ-saragih <bennefit.19@gmail.com>
2024-03-12 15:20:03 +0700

    CREATED: Tampilan menu dashboard

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.master')
+
+@section('content')
+<h1>Halaman dashboard</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,7 +19,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/customers', [CustomerController::class, 'index']);
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+Route::get('/customers', [CustomerController::class, 'index'])->name('customers.index');
 Route::post('/customers', [CustomerController::class, 'store'])->name('customers.store');
 Route::get('/customers/create', [CustomerController::class, 'create'])->name('customers.create');
 Route::get('/customers/{id}/edit', [CustomerController::class, 'edit'])->name('customers.edit');
